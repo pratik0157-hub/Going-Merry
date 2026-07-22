61e70b38c5eabda1da6ce4808767f03386777c7a
pratik0157-hub <pratik0157@gmail.com>
2026-07-22 22:38:32 +0530

    revision

--- python/functions.py
+++ python/functions.py
@@ -40,3 +40,14 @@ class Solution:
                 if nums[i] + nums[j] == target:
                     return [i, j]
         return []
+    
+
+def printMover(fr, to):
+    print("Move disk from rod", str(fr), "to rod", str(to))
+def Towers(n, fr, to, spare):
+    if n == 1:
+        printMover(fr, to)
+    else:
+        Towers(n - 1, fr, spare, to)
+        Towers(1, fr, to, spare)
+        Towers(n - 1, spare, to, fr)
