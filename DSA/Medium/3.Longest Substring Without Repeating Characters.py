b1c314017ad849dbc849dc999956aef030264fd0
pratik0157-hub <pratik0157@gmail.com>
2026-07-24 22:15:57 +0530

    third problem

---
+++ DSA/Medium/3.Longest Substring Without Repeating Characters.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python3
+
+"""
+Given a string s, find the length of the longest without duplicate characters.
+
+ 
+
+Example 1:
+
+Input: s = "abcabcbb"
+Output: 3
+Explanation: The answer is "abc", with the length of 3. Note that "bca" and "cab" are also correct answers.
+
+Example 2:
+
+Input: s = "bbbbb"
+Output: 1
+Explanation: The answer is "b", with the length of 1.
+
+Example 3:
+
+Input: s = "pwwkew"
+Output: 3
+Explanation: The answer is "wke", with the length of 3.
+Notice that the answer must be a substring, "pwke" is a subsequence and not a substring.
+
+ 
+
+Constraints:
+
+    0 <= s.length <= 5 * 104
+    s consists of English letters, digits, symbols and spaces.
+
+
+"""
+My solution:
+
+class Solution:
+    def lengthOfLongestSubstring(self, s):
+        l = 0
+        foo = set()
+        max_length = 0
+        for r in range(len(s)):
+            while s[r] in foo:
+                foo.remove(s[l])
+                l += 1
+            foo.add(s[r])
+            max_length = max(max_length, r - l + 1)
+
+        return max_length
+        
+        
+
+        
