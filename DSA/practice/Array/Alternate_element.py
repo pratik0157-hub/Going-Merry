58faeb18cab78e53b71fad469baf91b4ce1a08d3
pratik0157-hub <pratik0157@gmail.com>
2026-07-28 23:03:23 +0530

    Array basics

--- DSA/practice/Array/Alternate_element.py
+++ DSA/practice/Array/Alternate_element.py
@@ -16,3 +16,25 @@ if __name__ == "__main__":
     arr = [10, 20, 30, 40, 50]
     res = getAlternates(arr)
     print(" ".join(map(str, res)))
+
+
+
+"""advanced!!"""
+# Recursive Python Program to print alternate elements
+# of the array
+
+# Recursive function to store all alternate elements
+def getAlternatesRec(arr, idx, res):
+    if idx < len(arr):
+        res.append(arr[idx])
+        getAlternatesRec(arr, idx + 2, res)
+
+def getAlternates(arr):
+    res = []
+    getAlternatesRec(arr, 0, res)
+    return res
+
+if __name__ == "__main__":
+    arr = [10, 20, 30, 40, 50]
+    res = getAlternates(arr)
+    print(" ".join(map(str, res)))
